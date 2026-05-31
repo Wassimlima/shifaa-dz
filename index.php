ccc36e2b1c07d3fe11844293077a63eb952d855e
Wassimlima <149905849+Wassimlima@users.noreply.github.com>
2026-05-31 21:45:27 +0100

    Initial commit - Shifaa DZ full project

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php session_start(); require 'includes/config.php'; ?>
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+<head><title>شفاء</title></head>
+<body>
+<h1>مرحبا في شفاء - 69 ولاية</h1>
+<a href="login.php">تسجيل الدخول</a>
+</body>
+</html>
